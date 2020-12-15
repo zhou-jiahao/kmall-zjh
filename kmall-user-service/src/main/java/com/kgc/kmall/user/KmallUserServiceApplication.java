089910c47151f2b46c800206f5f24b9c35372fb4
zhoujiahao_glb <1836418597@qq.com>
2020-12-15 16:05:41 +0800

    kmall-user-service

---
+++ kmall-user-service/src/main/java/com/kgc/kmall/user/KmallUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kgc.kmall.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KmallUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KmallUserServiceApplication.class, args);
+	}
+
+}
