8070129f4fca52d8b549dad9ac3155a31d31273b
zhoujiahao_glb <1836418597@qq.com>
2020-12-17 18:25:42 +0800

    销售id和销售值

---
+++ kmall-manager-web/src/main/java/com/kgc/kmall/manager/controller/SpuController.java
@@ -0,0 +1,43 @@
+package com.kgc.kmall.manager.controller;
+
+import com.kgc.kmall.bean.PmsBaseSaleAttr;
+import com.kgc.kmall.bean.PmsProductInfo;
+import com.kgc.kmall.service.SpuService;
+import org.apache.dubbo.config.annotation.Reference;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@CrossOrigin
+@RestController
+public class SpuController {
+
+    @Reference
+    SpuService spuService;
+
+    @RequestMapping("/spuList")
+    public List<PmsProductInfo> pmsProductInfo(Long catalog3Id){
+        return spuService.spuList(catalog3Id);
+    }
+    @RequestMapping("/fileUpload")
+    public String fileUpload(@RequestParam("file")MultipartFile file){
+        //文件上传
+        //返回文件上传后的路径
+        return "https://m.360buyimg.com/babel/jfs/t5137/20/1794970752/352145/d56e4e94/591417dcN4fe5ef33.jpg";
+    }
+
+    @RequestMapping("/baseSaleAttrList")
+    public List<PmsBaseSaleAttr> baseSaleAttrList(){
+        List<PmsBaseSaleAttr> saleAttrList = spuService.baseSaleAttrList();
+        return saleAttrList;
+    }
+
+    @RequestMapping("/saveSpuInfo")
+    public String saveSpuInfo(@RequestBody PmsProductInfo pmsProductInfo){
+        return "success";
+    }
+
+}
---
+++ kmallapi/src/main/java/com/kgc/kmall/bean/PmsProductSaleAttr.java
@@ -0,0 +1,57 @@
+package com.kgc.kmall.bean;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class PmsProductSaleAttr implements Serializable {
+    private Long id;
+
+    private Long productId;
+
+    private Long saleAttrId;
+
+    private String saleAttrName;
+
+    //销售属性值集合
+    private List<pmsProductSaleAttrValue> spuSaleAttrValueList;
+
+    public List<pmsProductSaleAttrValue> getSpuSaleAttrValueList() {
+        return spuSaleAttrValueList;
+    }
+
+    public void setSpuSaleAttrValueList(List<pmsProductSaleAttrValue> spuSaleAttrValueList) {
+        this.spuSaleAttrValueList = spuSaleAttrValueList;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public Long getSaleAttrId() {
+        return saleAttrId;
+    }
+
+    public void setSaleAttrId(Long saleAttrId) {
+        this.saleAttrId = saleAttrId;
+    }
+
+    public String getSaleAttrName() {
+        return saleAttrName;
+    }
+
+    public void setSaleAttrName(String saleAttrName) {
+        this.saleAttrName = saleAttrName == null ? null : saleAttrName.trim();
+    }
+}
