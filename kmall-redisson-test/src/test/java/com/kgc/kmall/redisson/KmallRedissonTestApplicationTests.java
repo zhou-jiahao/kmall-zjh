26e31e2eb6875bbe9fa454ba7409669f07abe9f3
zhoujiahao_glb <1836418597@qq.com>
2021-01-05 10:09:47 +0800

    完成缓存击穿、穿透、雪崩、和search的web和service

---
+++ kmall-redisson-test/src/test/java/com/kgc/kmall/redisson/KmallRedissonTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kgc.kmall.redisson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KmallRedissonTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
